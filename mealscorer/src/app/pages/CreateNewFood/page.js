0bc8aad1230f4c6d5655f5d2a3b2badb105fe30c
JC <jcsmileyjr@gmail.com>
2024-01-04 21:30:58 -0600

    Feature: Create New Food page

--- mealscorer/src/app/pages/CreateNewFood/page.js
+++ mealscorer/src/app/pages/CreateNewFood/page.js
@@ -1,9 +1,30 @@
 import styles from './createNewFood.module.css';
+import Header from '@/component/Header/page';
+import Link from "next/link";
 
 export default async function createNewFood ({}) {
     return(
         <main className={styles.main}>
-            testing
+            <Header />
+            <section className={styles.contentContainer}>
+                <div className={styles.inputContainer}>
+                    <label className={styles.labelField} htmlFor='nameInputField'>Name</label>
+                    <input type="text" className={`${styles.dataField}`} id="nameInputField" />
+                </div>
+            </section>
+            <section className={styles.contentContainer}>
+                <div className={styles.inputContainer}>
+                    <label className={styles.labelField} htmlFor='typeField'>Type</label>
+                    <select className={`${styles.dataField} ${styles.selectField}`} id="typeField">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                </div>
+            </section>
+            <section className={styles.contentContainer}>
+                <button className={styles.doneButton} type="button">Done</button>
+            </section>
         </main>
     )
 }
---
+++ mealscorer/src/component/NameInputField/page.js
@@ -0,0 +1,10 @@
+import styles from './nameInputField.module.css';
+
+export default async function NameInputField () {
+    return (
+        <div>
+            <label></label>
+            <input type="text" style={} />
+        </div>
+    )
+}
